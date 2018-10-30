4ef64aa080892abdf25137abec3378ee460b564b
hugo valois <hvgaming@yahoo.com>
2018-10-30 00:53:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,15 +2,5 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri' #=> I added this one
 require 'rubygems' #=> I added this one
-class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
 
